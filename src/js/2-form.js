0b1bdec01301d554fdb9b7917b698784d358f139
irpolg <irpolg@ukr.net>
2024-03-25 20:52:28 +0200

    9hw-upd-form-to-ment2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,8 +1,14 @@
-const LOCAL_KEY = "feedback-form-stage"; //new
-const form = document.querySelector(".feedback-form"); //new
+// Завдання 2 - Форма зворотного зв'язку
+// Виконуй це завдання у файлах 2-form.html і 2-form.js.
+// Додай у HTML розмітку форми.У JS напиши скрипт,
+//     який буде зберігати значення полів у локальне сховище,
+//     коли користувач щось друкує.
+
+const LOCAL_KEY = "feedback-form-stage"; 
+const form = document.querySelector(".feedback-form"); 
 
 form.addEventListener("submit", handleSubmit);
-form.addEventListener("input", onTextInput); //new
+form.addEventListener("input", onTextInput); 
 reload();
 
 //Отримуємо значення поля. Зберігаємо його в сховищі
@@ -35,58 +41,8 @@ function handleSubmit(event) {
   event.preventDefault();
   const { email, message } = event.currentTarget.elements; // new
   console.log({ email: email.value, message: message.value }); //new
-  console.log("handleSubmit :", event);
-  console.log("Ваше повідомлення відправлено");
+  // console.log("handleSubmit :", event);
+  // console.log("Ваше повідомлення відправлено");
   localStorage.removeItem(LOCAL_KEY); //new
   event.currentTarget.reset();
 }
-
-
-
-// const LOCAL_KEY_EMAIL = "feedback-form-email";
-// const LOCAL_KEY_MSG   = "feedback-form-msg";
-// const inputEmail   = document.querySelector(".form-email");
-// const inputMessage = document.querySelector("textarea");
-// const form = document.querySelector(".feedback-form");
-
-// form.addEventListener("submit", handleSubmit);
-// inputEmail.addEventListener("input", onEmailInput);
-// reload();
-
-// inputMessage.addEventListener("input", onMessageInput);
-// reload();
-
-// //Отримуємо значення поля. Зберігаємо його в сховищі
-// function onEmailInput(event) {
-//     const emailText = event.target.value;
-//     localStorage.setItem(LOCAL_KEY_EMAIL, emailText);
-// }
-
-// //Отримуємо значення поля. Зберігаємо його в сховищі
-// function onMessageInput(event) {
-//     const messageText = event.target.value;
-//     localStorage.setItem(LOCAL_KEY_MSG, messageText);
-// } 
-
-// //Отримуємо дані зі сховища. Якщо там щось було, то оновлюємо DOM 
-// //значення зі сховища з'являються в полях на екрані форми
-// function reload() {
-//     const savedEmail = localStorage.getItem(LOCAL_KEY_EMAIL);
-//     if (savedEmail) {
-//         inputEmail.value = savedEmail;
-//     }
-//         const savedMessage = localStorage.getItem(LOCAL_KEY_MSG);
-//     if (savedMessage) {
-//         inputMessage.value = savedMessage;
-//     }
-// }
-
-// //Відправляємо форму та видаляємо ключі з localStorage
-// function handleSubmit(event) {
-//     event.preventDefault();
-//     console.log("handleSubmit :", event);
-//     console.log("Ваше повідомлення відправлено");
-//     localStorage.removeItem(LOCAL_KEY_EMAIL);
-//     localStorage.removeItem(LOCAL_KEY_MSG);
-//     event.currentTarget.reset();
-// }
